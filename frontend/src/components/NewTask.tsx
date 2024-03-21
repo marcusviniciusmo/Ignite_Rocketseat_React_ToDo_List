b784f6b5a467f81cd6177c6713b09fceb4d93754
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2024-03-21 18:45:46 +0000

    Logic to create a new task

--- frontend/src/components/NewTask.tsx
+++ frontend/src/components/NewTask.tsx
@@ -1,15 +1,37 @@
+import { useState } from 'react';
 import { AddCircleOutlineOutlined } from '@mui/icons-material';
+import { Task } from './Tasks';
 import styles from './NewTask.module.css';
 
-export function NewTask() {
+interface NewTaskProps {
+  tasks: Task[];
+  setTasks: Function;
+};
+
+export function NewTask({ tasks, setTasks }: NewTaskProps) {
+  // const [tasks, setTasks] = useState<string[]>([]);
+  const [newTask, setNewTask] = useState<string>('');
+
+  const handleNewTask = () => {
+    setTasks([...tasks, {
+      id: tasks.length + 1,
+      description: newTask,
+      status: 'created'
+    }]);
+    setNewTask('');
+  };
+
+
   return (
     <div className={styles.newTask}>
       <input
         type="text"
         className={styles.newTaskInput}
         placeholder='Adicione uma nova tarefa'
+        value={newTask}
+        onChange={e => setNewTask(e.target.value)}
       />
-      <button className={styles.newTaskButton}>
+      <button className={styles.newTaskButton} onClick={handleNewTask}>
         <span className={styles.newTaskButtonLabel}>Criar</span>
         <AddCircleOutlineOutlined />
       </button>
--- frontend/src/components/Tasks.tsx
+++ frontend/src/components/Tasks.tsx
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import { NewTask } from './NewTask';
 import { Empty } from './Empty';
 import styles from './Tasks.module.css';
 
+export interface Task {
+  id: number;
+  description: string;
+  status: 'created' | 'done' | 'deleted';
+};
+
 export function Tasks() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
   return (
     <div>
-      <NewTask />
+      <NewTask tasks={tasks} setTasks={setTasks} />
 
       <div className={styles.tasks}>
         <div className={styles.info}>
@@ -19,7 +28,15 @@ export function Tasks() {
           </div>
         </div>
 
-        <Empty />
+        <>
+          {
+            tasks.length > 0
+              ? tasks.map(task => {
+                return <>{task.description}</>
+              })
+              : <Empty />
+          }
+        </>
       </div>
     </div>
   )
