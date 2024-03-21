af5b9b410bf91c2897d24028330b43e7878d4c20
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2024-03-21 12:17:13 +0000

    NewTask component inside Tasks component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,10 @@
 import { Header } from "./components/Header";
-import { NewTask } from "./components/NewTask";
 import { Tasks } from "./components/Tasks";
 
 export function App() {
   return (
     <main>
       <Header />
-
-      <NewTask />
       
       <Tasks />
     </main>
--- frontend/src/components/Tasks.tsx
+++ frontend/src/components/Tasks.tsx
@@ -1,21 +1,26 @@
+import { NewTask } from './NewTask';
 import { Empty } from './Empty';
 import styles from './Tasks.module.css';
 
 export function Tasks() {
   return (
-    <div className={styles.tasks}>
-      <div className={styles.info}>
-        <div className={styles.created}>
-          <label>Tarefas criadas</label>
-          <span className={styles.counter}>0</span>
-        </div>
-        <div className={styles.done}>
-          <label>Concluídas</label>
-          <span className={styles.counter}>0</span>
-        </div>
-      </div>
+    <div>
+      <NewTask />
 
-      <Empty />
+      <div className={styles.tasks}>
+        <div className={styles.info}>
+          <div className={styles.created}>
+            <label>Tarefas criadas</label>
+            <span className={styles.counter}>0</span>
+          </div>
+          <div className={styles.done}>
+            <label>Concluídas</label>
+            <span className={styles.counter}>0</span>
+          </div>
+        </div>
+
+        <Empty />
+      </div>
     </div>
   )
 };
